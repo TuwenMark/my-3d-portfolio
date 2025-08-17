b5ab333e261449222e2ead4752b579c28c1e016e
MarkTuwen <m18698577021@163.com>
2025-08-17 11:50:04 +0800

    add Navbar section

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Navbar from './sections/Navbar'
+
+export default function Home() {
+  return (
+    <div className="container mx-auto max-w-7xl">
+      {/* navbar */}
+      <Navbar />
+      {/* hero */}
+      {/* about */}
+      {/* projects */}
+      {/* experience */}
+      {/* testimonial */}
+      {/* contact */}
+      {/* footer */}
+    </div>
+  )
+}
